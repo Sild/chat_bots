d59d92539c358007675a139dc6b94d1bb4418227
Dmitrii Korchagin <d.korchagin@ston.fi>
2024-11-15 00:31:51 +0100

    rm outdated projecta

--- slack_cmd_core/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod slack_cmd;
-mod handler;
-mod slack_helper;
-mod state;
-
-pub use slack_cmd::run;
-pub use handler::{MessageHandler, ALL_CHANNELS};
-pub use handler::handlers;
-pub use state::HandlerContext;
